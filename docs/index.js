71e0a048fd5acc04ff9cc01990c9d8196bf0431d
Andy Jiang <andyjiang@gmail.com>
2017-06-26 11:51:17 -0700

    add tracking

---
+++ docs/index.js
@@ -0,0 +1,28 @@
+
+/**
+ * Analytics.
+ */
+
+window.addEventListener('click', (e) => {
+  if (e.target.hasAttribute('data-track-link')) {
+    trackLink(e.target);
+  }
+
+  // no, get out!
+  return;
+});
+
+/**
+ * trackLink
+ * Track the click of a link in page body copy
+ * @param  {Node} anchor - the clicked anchor
+ */
+
+function trackLink(anchor) {
+  window.analytics.track('Link Clicked', {
+    href: anchor.href,
+    location: anchor.getAttribute('data-link-location'),
+    style: anchor.getAttribute('data-link-style'),
+    text: anchor.title
+  });
+}
